13c8a0bee372d8a000c4b0244c13bb63310d0367
eliasmorio <eliasmorio@users.noreply.github.com>
2025-07-11 18:44:30 +0200

    Init repo with npx giget@latest gh:adonisjs/pkg-starter-kit

---
+++ providers/zod_provider.ts
@@ -0,0 +1,58 @@
+/*
+ * @eliasmorio/zod
+ *
+ * (c) Elias Morio
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import type { ApplicationService } from '../src/types.js'
+import { Request, RequestValidator } from '../modules/http/main.js'
+import { FileRuleValidationOptions, VineMultipartFile } from '../src/vine.js'
+
+/**
+ * Extend VineJS
+ */
+declare module '@vinejs/vine' {
+  interface Vine {
+    file(options?: FileRuleValidationOptions): VineMultipartFile
+  }
+}
+
+/**
+ * Extend HTTP request class
+ */
+declare module '@adonisjs/core/http' {
+  interface Request extends RequestValidator {}
+}
+
+/**
+ * The Edge service provider configures Edge to work within
+ * an AdonisJS application environment
+ */
+export default class VineJSServiceProvider {
+  constructor(protected app: ApplicationService) {
+    this.app.usingVineJS = true
+  }
+
+  boot() {
+    const experimentalFlags = this.app.experimentalFlags
+
+    /**
+     * The file method is used to validate a field to be a valid
+     * multipart file.
+     */
+    Vine.macro('file', function (this: Vine, options) {
+      return new VineMultipartFile(options)
+    })
+
+    /**
+     * The validate method can be used to validate the request
+     * data for the current request using VineJS validators
+     */
+    Request.macro('validateUsing', function (this: Request, ...args) {
+      return new RequestValidator(this.ctx!, experimentalFlags).validateUsing(...args)
+    })
+  }
+}
